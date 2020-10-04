b41c428986926328cd5545edcddcad29864af0e1
andresns <andresm.navarro@gmail.com>
2020-10-04 18:53:30 -0400

    add buttons to guess the square color and check answer

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const rows = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const generateButton = document.querySelector(".generate-button");
+const square = document.querySelector(".square");
 const answer = document.querySelector(".answer");
+const blackButton = document.querySelector(".black");
+const whiteButton = document.querySelector(".white");
+let currentSquare = {};
 
-generateButton.addEventListener("click", ()=>{
+window.onload = function () {
+    currentSquare = getRandomSquare();
+    square.textContent = currentSquare.name;
+}
 
-    const col = columns[getRandomInt(8)];
-    const row = rows[getRandomInt(8)];
-    answer.textContent = `${col}${row}`;
+blackButton.addEventListener("click", () => {
+    if (currentSquare.color === "black") {
+        answer.textContent = "Correct!";
+    } else {
+        answer.textContent = "Wrong!";
+    }
+    currentSquare = getRandomSquare();
+    square.textContent = currentSquare.name;
 });
 
-function getRandomInt(max){
+whiteButton.addEventListener("click", () => {
+    if (currentSquare.color === "white") {
+        answer.textContent = "Correct!";
+    } else {
+        answer.textContent = "Wrong!";
+    }
+    currentSquare = getRandomSquare();
+    square.textContent = currentSquare.name;
+});
+
+function getRandomSquare() {
+    const colIndex = getRandomInt(8);
+    const rowIndex = getRandomInt(8);
+    const col = columns[colIndex];
+    const row = rows[rowIndex];
+    let squareColor = "";
+
+    if ((colIndex + rowIndex) % 2 == 0) {
+        squareColor = "black";
+    } else {
+        squareColor = "white";
+    }
+    return {
+        name: `${col}${row}`,
+        color: squareColor
+    };
+}
+
+function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
