5559dd4a4950493986c51d551847a3fb40ebb494
Andres Navarro S <andresm.navarro@gmail.com>
2022-07-10 14:28:04 -0400

    Update open all shortcuts button to close current tab after opening all shortcuts

--- src/components/widgets/Shortcuts/index.jsx
+++ src/components/widgets/Shortcuts/index.jsx
@@ -7,23 +7,26 @@ import ShortcutIcon from "../../modules/ShortcutIcon";
 // Data
 import config from "../../../homepage-config.json";
 
+const shortcuts = config.shortcuts.filter((shortcut) => shortcut.enabled);
+
 const Shortcuts = () => {
-  const handleOpenAllClick = () =>
-    config.shortcuts.map((shortcut) => window.open(shortcut.href, "_blank"));
+  const handleOpenAllClick = () => {
+    shortcuts.map((shortcut) => window.open(shortcut.href, "_blank"));
+    window.open("about:blank", "_self");
+    window.close();
+  };
 
   return (
     <div className="shortcuts">
       <div className="shortcuts__icons">
-        {config.shortcuts
-          .filter((shortcut) => shortcut.enabled)
-          .map((shortcut, index) => (
-            <ShortcutIcon
-              name={shortcut.name}
-              icon={shortcut.icon}
-              href={shortcut.href}
-              key={index}
-            />
-          ))}
+        {shortcuts.map((shortcut, index) => (
+          <ShortcutIcon
+            name={shortcut.name}
+            icon={shortcut.icon}
+            href={shortcut.href}
+            key={index}
+          />
+        ))}
       </div>
       <div className="shortcuts__open-all">
         <button onClick={handleOpenAllClick}>Open all</button>
