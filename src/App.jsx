d6833b799ddc9f5db8435176dde4df6ff629400f
Andres Navarro S <andresm.navarro@gmail.com>
2023-11-10 10:17:48 -0300

    Remove To do list widget

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 // Components
 import SearchBar from "./components/widgets/SearchBar";
 import Shortcuts from "./components/widgets/Shortcuts";
-import ToDoList from "./components/widgets/ToDoList";
+// import ToDoList from "./components/widgets/ToDoList";
 import MTGACodes from "./components/widgets/MTGACodes";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <SearchBar className="root__search-bar" />
         <Shortcuts />
         <MTGACodes />
-        <ToDoList />
+        {/* <ToDoList /> */}
       </div>
     </div>
   );
